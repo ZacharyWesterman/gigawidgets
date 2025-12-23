29de6e91370e46bd4c3eb2267fb9f8c44db6d37c
Zachary Westerman <westerman.zachary@gmail.com>
2025-12-22 20:14:51 -0600

    Handle touch events in emulator

--- emu/arduino/Arduino_GigaDisplayTouch.cpp
+++ emu/arduino/Arduino_GigaDisplayTouch.cpp
@@ -1,5 +1,7 @@
 #include "Arduino_GigaDisplayTouch.h"
 
+#include <raylib.h>
+
 Arduino_GigaDisplayTouch::Arduino_GigaDisplayTouch() {}
 
 Arduino_GigaDisplayTouch::~Arduino_GigaDisplayTouch() {}
@@ -9,5 +11,13 @@ bool Arduino_GigaDisplayTouch::begin() {
 }
 
 uint8_t Arduino_GigaDisplayTouch::getTouchPoints(GDTpoint_t *points) {
+	// Only supports a single touch point.
+
+	if (IsMouseButtonDown(0)) {
+		points[0].x = GetMouseX();
+		points[0].y = GetMouseY();
+		return 1;
+	}
+
 	return 0;
 }
