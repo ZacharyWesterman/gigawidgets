abf90b749be4a1bf4a8c3290d291550983160a01
Zachary Westerman <westerman.zachary@gmail.com>
2025-12-29 22:03:42 -0600

    fix text sometimes segfaulting, and fix rendering sometimes failing for empty text

--- src/ui/text.cpp
+++ src/ui/text.cpp
@@ -5,6 +5,9 @@
 
 namespace ui {
 
+/// @brief A shared placeholder text for rendering blank text.
+static const String dummyText(" ");
+
 Text::Text(const String &text, const GFXfont *font, color_t color, const Position &pos, const Alignment &align, bool wrap) : text(text), font(font), scale(1), textColor(color), wrap(wrap), Widget(pos, align) {}
 
 Text::Text(const String &text, fontsize_t scale, color_t color, const Position &pos, const Alignment &align, bool wrap) : text(text), font(nullptr), scale(scale), textColor(color), wrap(wrap), Widget(pos, align) {}
@@ -34,20 +37,14 @@ void Text::draw() const {
 }
 
 Size Text::size() const {
-	const auto p = parentBounds();
-	const auto width = p.max.x - p.min.x;
-	const auto height = p.max.y - p.min.y;
-
-	const uint8_t y_offset = font ? font->yAdvance / 2 : 0;
-
-	GFXcanvas1 canvas(width, height);
+	GFXcanvas1 canvas(ui::width(), ui::height());
 	canvas.setTextSize(scale);
 	canvas.setTextWrap(wrap);
 	canvas.setFont(font);
 
 	int16_t x, y;
 	uint16_t w, h;
-	canvas.getTextBounds(text, 0, 0, &x, &y, &w, &h);
+	canvas.getTextBounds(text.length() ? text : dummyText, 0, 0, &x, &y, &w, &h);
 
 	return {
 		w + 5,
