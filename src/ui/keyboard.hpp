54ca37f7a230d29bc329a59a3c19bb09321b5012
Zachary Westerman <westerman.zachary@gmail.com>
2026-04-20 12:01:31 -0500

    Remove unused keyboard methods

--- src/ui/keyboard.hpp
+++ src/ui/keyboard.hpp
@@ -33,36 +33,6 @@ public:
 	 * @param backgroundColor The background color.
 	 */
 	Keyboard(const GFXfont &font, color_t textColor, color_t buttonColor, color_t backgroundColor);
-
-	/**
-	 * @brief Set the font to render text in.
-	 * @param font The new font to render the text in. If null, defaults to the built-in font.
-	 */
-	void setFont(GFXfont *const font);
-
-	/**
-	 * @brief Set the scaling factor for the text.
-	 * @param scale The new scaling factor for the text, e.g. 2x, 3x, etc.
-	 */
-	void setScale(fontsize_t scale);
-
-	/**
-	 * @brief Set the text color.
-	 * @param newColor The new text color.
-	 */
-	void setTextColor(color_t newColor);
-
-	/**
-	 * @brief Set the button color.
-	 * @param newColor The new button color.
-	 */
-	void setButtonColor(color_t newColor);
-
-	/**
-	 * @brief Set the background color.
-	 * @param newColor The new background color.
-	 */
-	void setBackgroundColor(color_t newColor);
 };
 
 } // namespace ui
