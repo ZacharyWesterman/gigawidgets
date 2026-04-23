127fcdb0b64bf284f14238e10850f22c21bacd5f
Zachary Westerman <westerman.zachary@gmail.com>
2026-04-22 22:37:41 -0500

    re-render fixes

--- src/ui/containers/box.cpp
+++ src/ui/containers/box.cpp
@@ -6,25 +6,13 @@ namespace ui {
 Box::Box(Widget *child, const Bounds &bounds) : widgetBounds(bounds), SingleChildWidget(child) {}
 
 Bounds Box::bounds() const {
-	Coords min = parentBounds().min;
-	Coords max = {
-		widgetBounds.max.x - widgetBounds.min.x + min.x,
-		widgetBounds.max.y - widgetBounds.min.y + min.y,
-	};
-
-	return {min, max};
+	return widgetBounds;
 }
 
 void Box::setBounds(const Bounds &bounds) {
 	widgetBounds = bounds;
-	Serial.print(bounds.min.x);
-	Serial.print(',');
-	Serial.print(bounds.min.y);
-	Serial.print(' ');
-	Serial.print(bounds.max.x);
-	Serial.print(',');
-	Serial.print(bounds.max.y);
-	Serial.println(';');
+	redrawSelf = true;
+	redrawParent = true;
 }
 
 void Box::draw() const {
