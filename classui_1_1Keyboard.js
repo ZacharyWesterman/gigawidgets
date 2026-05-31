6bb900a10ddbfec2ce546e667b87fa7f4da9b087
ZacharyWesterman <ZacharyWesterman@users.noreply.github.com>
2026-05-31 16:23:32 +0000

    deploy: 2920deda9703799f872f197d4ab8f984a20cfce9

--- classui_1_1Keyboard.js
+++ classui_1_1Keyboard.js
@@ -1,5 +1,5 @@
 var classui_1_1Keyboard =
 [
-    [ "Keyboard", "classui_1_1Keyboard.html#acb9c1daabd752aa1be00d8b47a36927a", null ],
+    [ "Keyboard", "classui_1_1Keyboard.html#a1e7b418ebd72e4707236c52567eb0424", null ],
     [ "Keyboard", "classui_1_1Keyboard.html#a65de64b1b83140f77a2b67c52db4bf82", null ]
 ];
--- search/functions_9.js
+++ search/functions_9.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['keyboard_0',['keyboard',['../classui_1_1Keyboard.html#acb9c1daabd752aa1be00d8b47a36927a',1,'ui::Keyboard::Keyboard(const layout::KeyboardLayout &amp;layout, fontsize_t scale, color_t textColor, color_t buttonColor, color_t backgroundColor)'],['../classui_1_1Keyboard.html#a65de64b1b83140f77a2b67c52db4bf82',1,'ui::Keyboard::Keyboard(const layout::KeyboardLayout &amp;layout, const GFXfont &amp;font, color_t textColor, color_t buttonColor, color_t backgroundColor)']]]
+  ['keyboard_0',['keyboard',['../classui_1_1Keyboard.html#a1e7b418ebd72e4707236c52567eb0424',1,'ui::Keyboard::Keyboard(const layout::KeyboardLayout &amp;layout, color_t textColor, color_t buttonColor, color_t backgroundColor)'],['../classui_1_1Keyboard.html#a65de64b1b83140f77a2b67c52db4bf82',1,'ui::Keyboard::Keyboard(const layout::KeyboardLayout &amp;layout, const GFXfont &amp;font, color_t textColor, color_t buttonColor, color_t backgroundColor)']]]
 ];
